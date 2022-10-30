7306002ac745bd477a16f83eccae46d0d0029cb4
PetrovicArmin <apetrovic1@etf.unsa.ba>
2022-10-30 07:42:40 +0100

    This is my second feature on test branch!

--- OnlineVoting/Program.cs
+++ OnlineVoting/Program.cs
@@ -8,6 +8,7 @@ namespace OnlineVoting
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("This is additional line in test branch! (feature)!");
+            Console.WriteLine("This is change in test branch! - added new feature!");
         }
     }
 }
