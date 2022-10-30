4adeecf213cae32f97e943fe47d4427122cfabba
PetrovicArmin <apetrovic1@etf.unsa.ba>
2022-10-30 08:32:46 +0100

    This is starting commit for our project

--- OnlineVoting/Program.cs
+++ OnlineVoting/Program.cs
@@ -7,11 +7,6 @@ namespace OnlineVoting
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("This is additional line in test branch! (feature)!");
-            Console.WriteLine("This is someone else's line in master branch!");
-            Console.WriteLine("This is change in test branch! - added new feature!");
-            Console.WriteLine("This is final change in test branch!");
-            Console.WriteLine("This is my change in master!");
         }
     }
 }
