ad3793f8525d5362a2e34dac1a96435c2656203c
Petrović Armin <89392479+PetrovicArmin@users.noreply.github.com>
2022-10-30 09:54:37 +0100

    Update Program.cs

--- OnlineVoting/Program.cs
+++ OnlineVoting/Program.cs
@@ -8,6 +8,7 @@ namespace OnlineVoting
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("This is additional line in test branch! (feature)!");
+            Console.WriteLine("This is someone else's line in master branch!");
         }
     }
 }
