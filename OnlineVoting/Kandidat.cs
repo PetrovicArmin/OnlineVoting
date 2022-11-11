b6d9eff6c834dc3e0de86c94b0f781109e7758c7
npita1 <npita1@etf.unsa.ba>
2022-11-11 16:31:21 +0100

    Dodan interfejs Informacije na Kandidat klasu

--- OnlineVoting/Kandidat.cs
+++ OnlineVoting/Kandidat.cs
@@ -6,10 +6,22 @@ using System.Threading.Tasks;
 
 namespace OnlineVoting
 {
-    public class Kandidat : Osoba
+    public class Kandidat : Osoba, Informacije
     {
         public Kandidat(string ime, string prezime, string adresa, String datumRodjenja, string brojLicneKarte, long maticniBroj) : base(ime, prezime, adresa, datumRodjenja, brojLicneKarte, maticniBroj)
         {
+        }
+
+        public string DetaljneInformacije()
+        {
+            String detaljneInformacije = "";
+            return detaljneInformacije;
+        }
+
+        public string OsnovneInformacije()
+        {
+            String osnovneInformacije = "";
+            return osnovneInformacije;
         }
     }
 }
