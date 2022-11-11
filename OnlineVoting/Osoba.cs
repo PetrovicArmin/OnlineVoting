5f40a7ab82aae882e352005dfe15c64b510a2cc4
npita1 <npita1@etf.unsa.ba>
2022-11-11 16:09:52 +0100

    Prepravljena greska u Osoba klasi i dodano generisanje JIKa

--- OnlineVoting/Osoba.cs
+++ OnlineVoting/Osoba.cs
@@ -6,14 +6,35 @@ using System.Threading.Tasks;
 
 namespace OnlineVoting
 {
-    public abstract class Osoba
+    public class Osoba
     {
-        private String imeIPrezime { get; set; }
+        private String ime { get; set; }
+        private String prezime { get; set; }
         private String adresa { get; set; }
         private DateTime datumRodjenja { get; set; }
         private String brojLicneKarte { get; set; }
         private long maticniBroj { get; set; }
-        private String JIK { get; set; }
+        private String JIK { get; }
+
+        public Osoba (string ime, string prezime, string adresa, DateTime datumRodjenja, string brojLicneKarte, long maticniBroj)
+        {
+            this.ime = ime;
+            this.prezime = prezime;
+            this.adresa = adresa;
+            this.datumRodjenja = datumRodjenja;
+            this.brojLicneKarte = brojLicneKarte;
+            this.maticniBroj = maticniBroj;
+            this.JIK = generisiJIK(this.ime, this.prezime, this.adresa, this.datumRodjenja,this.brojLicneKarte,this.maticniBroj);
+        }
+
+        private String generisiJIK(String ime, String prezime, String adresa, DateTime datumRodjenja, String brojLicneKarte, long maticniBroj)
+        {
+            String jik = "";
+            jik = ime.Substring(0,2) + prezime.Substring(0,2) + adresa.Substring(0,2) + datumRodjenja.ToString("dd.MM.yyyy").Substring(0,2) + 
+                brojLicneKarte.Substring(0,2) + maticniBroj.ToString().Substring(0,2);
+            return jik;
+        }
+
 
     }
 }
--- OnlineVoting/Program.cs
+++ OnlineVoting/Program.cs
@@ -7,6 +7,8 @@ namespace OnlineVoting
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Osoba osoba = new Osoba("Ime", "Prezime", "Adresa", new DateTime(22-10-2022), "LICNAKARTA123",12345678910);
+            
         }
     }
 }
