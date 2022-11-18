f885ac57411a35a445c2ac75f65746597407a581
dterzic1 <dterzic1@etf.unsa.ba>
2022-11-18 18:40:22 +0100

    popravljen issue imenovanja u klasi GlasackiProces

--- OnlineVoting/GlasackiProces.cs
+++ OnlineVoting/GlasackiProces.cs
@@ -8,13 +8,13 @@ namespace OnlineVoting
 {
     internal class GlasackiProces
     {
-        private Osoba osoba { get; set; }   
-        private Glas glas { get; set; }
+        private Osoba Osoba { get; set; }   
+        private Glas Glas { get; set; }
 
         public GlasackiProces(Osoba osoba, Glas glas)
         {
-            this.osoba = osoba;
-            this.glas = glas;
+            this.Osoba = osoba;
+            this.Glas = glas;
             TipizirajIVratiGlas();
 
         }
@@ -22,7 +22,7 @@ namespace OnlineVoting
         public bool IdentifikujGlasaca()   
         {
             List<string> glasaci = Populacija.DajPopulaciju().getGlasaci();
-            if (glasaci.Contains(osoba.dajJIK()))   //ako je vec glasao, pada na ID fazi
+            if (glasaci.Contains(Osoba.dajJIK()))   //ako je vec glasao, pada na ID fazi
                 return false;
             return true;
         }
@@ -34,8 +34,8 @@ namespace OnlineVoting
 
         public Glas TipizirajIVratiGlas()
         {
-            int idStranke = glas.VratiIDStranke();
-            List<string> odabraniKandidati = glas.VratiIDKandidata(); //ID u smislu IDeve, mnozina.
+            int idStranke = Glas.VratiIDStranke();
+            List<string> odabraniKandidati = Glas.VratiIDKandidata(); //ID u smislu IDeve, mnozina.
             List<Stranka> stranke = Izbori.stranke;
             List<Kandidat> nezavisni = Izbori.kandidati;
             Stranka odabranaStranka = stranke.Find(s => s.vratiIdStranke() == idStranke);
@@ -45,25 +45,25 @@ namespace OnlineVoting
                     nezavisni.Where(k => k.dajJIK() == odabraniKandidati[0]).Any()
                     )
                 {
-                    glas.PostaviTipGlasa(TipGlasa.NEZAVISNI_KANDIDAT);
+                    Glas.PostaviTipGlasa(TipGlasa.NEZAVISNI_KANDIDAT);
                 }
-                else glas.PostaviTipGlasa(TipGlasa.NEVAZECI);
+                else Glas.PostaviTipGlasa(TipGlasa.NEVAZECI);
             }
             else
             {
                 if (odabraniKandidati == null || odabraniKandidati.Count() == 0)
-                    glas.PostaviTipGlasa(TipGlasa.SAMO_STRANKA);
+                    Glas.PostaviTipGlasa(TipGlasa.SAMO_STRANKA);
                 else
                 {   //da li su svi odabrani clanovi iz odabrane stranke
                     List<string> kandidatiOdabraneStranke = odabranaStranka.vratiClanove().ConvertAll(
                             new Converter<Kandidat, string>(k => k.dajJIK()));
                     if (ContainsAllItems(kandidatiOdabraneStranke, odabraniKandidati))
-                        glas.PostaviTipGlasa(TipGlasa.STRANKA_KANDIDATI);
+                        Glas.PostaviTipGlasa(TipGlasa.STRANKA_KANDIDATI);
                     else
-                        glas.PostaviTipGlasa(TipGlasa.NEVAZECI);
+                        Glas.PostaviTipGlasa(TipGlasa.NEVAZECI);
                 }
             }
-            return glas;
+            return Glas;
         }
 
 
